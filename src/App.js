1a6c34f8591509b13f42e8fa067b35e6b73c5bd3
ayrbox <sabin.dangol@hotmail.com>
2022-09-30 21:43:06 +0100

    Add background sound component with toggle button

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import { useMsgAfterSubmit } from './hooks'
 
 import './App.css'
 import HeroSvg from './components/HeroSvg'
+import bgSound from './assets/music/background-music.mp3'
+import BackgroundSound from './components/BackgroundSound'
 
 function App() {
   const [
@@ -98,6 +100,7 @@ function App() {
         <Picker.Item label="Multiplication(*)" value="multiplication" />
         <Picker.Item label="Division(/)" value="division" />
       </Picker>
+      <BackgroundSound url={bgSound} />
       <View style={styles.battlefield}>
         <View style={styles.container}>
           <View
---
+++ src/components/BackgroundSound.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Button } from 'react-native'
+
+/**
+ * NOTE: Potentialy better to use ogg sound format
+ */
+
+const BackgroundSound = ({ url }) => {
+  const [isReady, setReady] = useState(false)
+  const [isPlaying, setPlaying] = useState(false)
+  const audioRef = useRef(null)
+
+  useEffect(() => {
+    setReady(false)
+  }, [url])
+
+  useEffect(() => {
+    if (!audioRef.current) {
+      console.log('Audio not initialized yet')
+      return
+    }
+
+    // audioRef.current.play()
+  }, [audioRef])
+
+  const handleToggerPress = async () => {
+    if (!audioRef.current) return
+
+    if (isPlaying) {
+      await audioRef.current.pause()
+      setPlaying(false)
+      return
+    }
+
+    await audioRef.current.play()
+    setPlaying(true)
+  }
+
+  const handleCanPlayThrough = async () => {
+    setReady(true)
+
+    if (!audioRef.current) return
+
+    // try to auto play music browser allows
+    try {
+      await audioRef.current.play()
+    } catch (err) {
+      console.warn('Unable to auto play background sound.')
+      console.warn(err)
+    }
+  }
+
+  return (
+    <>
+      {isReady && (
+        <Button
+          onPress={handleToggerPress}
+          title={isPlaying ? 'Sound Off' : 'Sound On'}
+        />
+      )}
+      <audio ref={audioRef} onCanPlayThrough={handleCanPlayThrough}>
+        <source src={url} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </>
+  )
+}
+
+export default BackgroundSound
