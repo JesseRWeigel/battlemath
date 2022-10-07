64ecd16f3457175d04a7bb432d70afceb2aa95b9
ayrbox <sabin.dangol@hotmail.com>
2022-10-08 00:10:07 +0100

    Fix conflict.

--- src/App.tsx
+++ src/App.tsx
@@ -34,14 +34,10 @@ function App() {
 
   let submitInputRef = React.useRef<TextInput>(null)
 
-<<<<<<< HEAD:src/App.tsx
   const variablesToLookFor: [number, number] = [
     previousNumOfEnemies,
     numOfEnemies,
   ]
-=======
-  const variablesToLookFor = [previousNumOfEnemies, numOfEnemies]
->>>>>>> main:src/App.js
   const { msg, isErrorMessage } = useMsgAfterSubmit(
     variablesToLookFor,
     isStoredState
@@ -78,7 +74,6 @@ function App() {
 
   // Equivalent of componentDidMount
   useEffect(() => {
-<<<<<<< HEAD:src/App.tsx
     dispatch({ type: TYPES.NEW_PROBLEM })
     const storedData = localStorage.getItem('state')
     if (storedData) {
@@ -97,23 +92,6 @@ function App() {
           previousNumOfEnemies,
         })
       )
-=======
-    dispatch({ type: types.NEW_PROBLEM })
-    const storedData = localStorage.getItem('state')
-    if (storedData) {
-      dispatch({ type: types.RESTORE_STATE, payload: JSON.parse(storedData) })
-    } else {
-      localStorage.setItem('state', {
-        answer,
-        numOfEnemies,
-        val1,
-        val2,
-        won,
-        operator,
-        mode,
-        previousNumOfEnemies,
-      })
->>>>>>> main:src/App.js
     }
   }, [])
 
@@ -190,10 +168,6 @@ function App() {
         <View style={styles.container}>
           {[...Array(numOfEnemies)].map((_, i) => (
             <View
-<<<<<<< HEAD:src/App.tsx
-=======
-              className="enemy"
->>>>>>> main:src/App.js
               testID="enemies"
               key={i}
               style={[
